1dfb68cf59a4ff90111081ade1e0b09efd915cac
brgouveia2 <bruno.gouveiafarias@gmail.com>
2023-09-04 18:15:01 -0300

    components

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent {
 
+export class BigCardComponent {
+  
+  @Input()
+  photoCover: string="";
+  @Input()
+  cardTitle: string='NOVA VERSÃO DO ANGULAR JÁ SAIU!';
+  @Input()
+  cardDescription: string="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce maximus, diam sit amet ultricies varius, dui ipsum fringilla massa, vel facilisis ipsum sapien sed arcu. Donec porttitor nec est eu cursus.";
 }
